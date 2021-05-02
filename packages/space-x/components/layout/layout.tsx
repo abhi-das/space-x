87445706b55c6f576d84393f7cbc477213cdd094
abhishekdass08@gmail.com <abhishekdass08@gmail.com>
2021-05-02 23:21:04 +0530

    added footer

--- packages/space-x/components/layout/layout.tsx
+++ packages/space-x/components/layout/layout.tsx
@@ -1,19 +1,22 @@
 import React from 'react';
 import MainHeader from './main-header';
-
+import styles from '../../styles/Globals.module.scss';
 const Layout = (props) => {
     return <div>
-        <style jsx global>{`
+        {/* <style jsx global>{`
             body {
                 margin: 0;
                 padding: 0;
                 background: #e5e5e5;
             }
-        `}</style>
+        `}</style> */}
         <MainHeader />
         <main>
             {props.children}
         </main>
+        <footer className={styles.footer}>
+            <strong>Developed By:</strong>
+        </footer>
     </div>;
 }
 
