2e6e7737dfbd28ccaf6583f8e5a1fbc811e20a47
abhishekdass08@gmail.com <abhishekdass08@gmail.com>
2021-06-09 02:27:29 +0530

    updated url

--- packages/space-x/common/navigation-path.ts
+++ packages/space-x/common/navigation-path.ts
@@ -1,10 +1,6 @@
-const host = "https://space-x-apiv.herokuapp.com";
-const version = "v1";
-
 export const apiEndPoints = {
-  signIn: `${host}/${version}/signin`,
-  currentUser: `${host}/${version}/currentuser`,
-  launches: `${host}/${version}/launches`,
+  signIn: `https://space-x-apiv.herokuapp.com/v4/signin`,
+  currentUser: `https://space-x-apiv.herokuapp.com/v4/currentuser`,
 
   // signIn: `http://localhost:8011/v1/signin`,
   // currentUser: `http://localhost:8011/v1/currentuser`,
--- packages/space-x/helpers/api-utils.ts
+++ packages/space-x/helpers/api-utils.ts
@@ -1,5 +1,4 @@
 import { LaunchListItem } from "../components/launches/launch-list";
-import { apiEndPoints } from "../common/navigation-path";
 import axios from "axios";
 
 export interface ResError {
@@ -16,7 +15,7 @@ export async function getAllLaunch(): Promise<
     // const response = await axios.get(
     //   `https://space-lauch-default-rtdb.firebaseio.com/spaceX.json`
     // );
-    const response = await axios.get(apiEndPoints.launches, options);
+    const response = await axios.get("https://space-x-apiv.herokuapp.com/v4/launches", options);
     return response.data as Array<LaunchListItem>;
   } catch (err: any) {
     // TODO: Define Error Type at backend and export interface and use it here
