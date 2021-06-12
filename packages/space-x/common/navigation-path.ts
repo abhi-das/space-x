3bb0a0a1949bfab07eb0bf901f7006175d586e1f
abhishekdass08@gmail.com <abhishekdass08@gmail.com>
2021-06-12 16:10:46 +0530

    updated endpoint

--- packages/space-x/common/navigation-path.ts
+++ packages/space-x/common/navigation-path.ts
@@ -1,9 +1,10 @@
 export const apiEndPoints = {
-  // signIn: `https://space-x-apiv.herokuapp.com/v4/signin`,
-  // currentUser: `https://space-x-apiv.herokuapp.com/v4/currentuser`,
+  signIn: `https://space-x-apiv.herokuapp.com/v4/signin`,
+  signUp: `https://space-x-apiv.herokuapp.com/v4/signup`,
+  currentUser: `https://space-x-apiv.herokuapp.com/v4/currentuser`,
 
-  signIn: `http://localhost:8011/v4/signin`,
-  signUp: `http://localhost:8011/v4/signup`,
-  currentUser: `http://localhost:8011/v4/currentuser`,
-  launches: `http://localhost:8011/v4/launches`,
+  // signIn: `http://localhost:8011/v4/signin`,
+  // signUp: `http://localhost:8011/v4/signup`,
+  // currentUser: `http://localhost:8011/v4/currentuser`,
+  // launches: `http://localhost:8011/v4/launches`,
 };
